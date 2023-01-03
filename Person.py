14ef1bdbc326787ee0f10e960d5c61a7ff614567
Ben Cyna <benjamincyna@gmail.com>
2023-01-03 18:29:55 +1030

    person class created fully

--- Person.py
+++ Person.py
@@ -2,3 +2,17 @@ class Person:
  def __init__(self, name, skill):
     self.name = name
     self.skill = skill
+
+    def increment_skill(self):
+        self.skill += 5 if self.skill < 100 else 0
+
+    def decrement_skill(self):
+        self.skill -= 5 if self.skill > 5 else 0
+
+    def getSkill(self) -> int:
+        return self.skill
+    
+    def getName(self) -> str:   
+        return self.name
+
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+from Person import Person
+from Game import Game
+
+Will = Person("Will", 80)
+Ben = Person("Ben", 70)
+Finn = Person("Finnl", 70)
+Philip = Person("Philip", 50)
