9964e81cbd2abd45f8985744e7e2a89add949157
Ben Cyna <benjamincyna@gmail.com>
2023-01-11 17:51:37 +1030

    added winner functions in game

--- Game.py
+++ Game.py
@@ -4,6 +4,8 @@ class Game:
         self.p1 = player1
         self.p2 = player2
         self.__generateOdds
+        self.winner: Person = None
+        self.score = [0, 0]
     
     def getP1(self) -> Person:
         return self.p1
@@ -11,4 +13,15 @@ class Game:
     def getP2(self) -> Person:
         return self.p2
 
-    
+    def setWinner(self, player):
+        if player == self.p1 or player == self.p2:
+            self.winner = player
+            print("winner is" + self.winner.getName())
+        else:
+            print("Error, player is not in this game")
+
+    def getWinner(self):
+        if self.winner:
+            return "Winner"
+        else:
+            print("No winner yet!")
