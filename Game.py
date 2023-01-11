481b6b902151187e3070bad1417a30412ffdc6b8
Ben Cyna <benjamincyna@gmail.com>
2023-01-11 17:25:31 +1030

    added get players functions in game

--- Game.py
+++ Game.py
@@ -1,3 +1,14 @@
+from Person import Person
 class Game:
-    def __init__(self, ) -> None:
-        pass
+    def __init__(self, player1: Person, player2: Person) -> None:
+        self.p1 = player1
+        self.p2 = player2
+        self.__generateOdds
+    
+    def getP1(self) -> Person:
+        return self.p1
+
+    def getP2(self) -> Person:
+        return self.p2
+
+    
