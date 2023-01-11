fd4a19dcb8c7f1d5b5df92ac090d4995c29ad9a4
Ben Cyna <benjamincyna@gmail.com>
2023-01-11 17:59:27 +1030

    created number of rounds

--- Tournament.py
+++ Tournament.py
@@ -1,4 +1,5 @@
 from Person import Person
+import math
 class Tournament:
     def __init__(self, people: list[Person]) -> None:
         if len(people) < 4:
@@ -6,6 +7,7 @@ class Tournament:
             return
         self.players_len = len(people)
         self.players = people
+        self.rounds = 0
         self.__generateOdds()
         self.__generateGameTree()
 
@@ -17,8 +19,8 @@ class Tournament:
         for player in self.players:
             player.setOdds(player.getSkill()/skill_sum)
 
-    def __generateGameTree():
-        pass
+    def __generateGameTree(self):
+          self.rounds = math.ceil(math.log2(self.players_len))
         
     def showOdds(self) -> None:
         for player in self.players:
