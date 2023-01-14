b7b128519374da640e1017b011a59d72137694c0
Ben Cyna <benjamincyna@gmail.com>
2023-01-14 21:23:31 +1030

    GUI appearing

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from Tournament import Tournament
-
+import time
 class GUI:
     def __init__(self, tournament: Tournament) -> None:
         self.tournament = tournament
@@ -11,3 +11,5 @@ class GUI:
         window = Tk()
         window.config(padx=50, pady=50, width=900, height=700, background='white')
         window.title("Writers block block")
+        time.sleep(1)
+        window.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Person import Person
 from Tournament import Tournament
 from Game import Game
+from GUI import GUI
 
 # create people
 Will = Person("Will", 85)
@@ -9,7 +10,7 @@ Finn = Person("Finnl", 60)
 Philip = Person("Philip", 30)
 
 # start game
-ournament = Tournament([Will, Ben, Finn, Philip])
+tournament = Tournament([Will, Ben, Finn, Philip])
 #give gui the game
-
+gui = GUI(tournament)
 # tournament.showOdds() 
