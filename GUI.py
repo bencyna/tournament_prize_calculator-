d4314d80df41dab2d3718c6fab028a71bd7888bc
Ben Cyna <benjamincyna@gmail.com>
2023-01-14 21:21:47 +1030

    window gui created

--- GUI.py
+++ GUI.py
@@ -1,3 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+from Tournament import Tournament
+
 class GUI:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tournament: Tournament) -> None:
+        self.tournament = tournament
+        self.startGui()
+
+    def startGui(self):
+        window = Tk()
+        window.config(padx=50, pady=50, width=900, height=700, background='white')
+        window.title("Writers block block")
--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from Person import Person
 from Tournament import Tournament
 from Game import Game
 
+# create people
 Will = Person("Will", 85)
 Ben = Person("Ben", 60)
 Finn = Person("Finnl", 60)
 Philip = Person("Philip", 30)
-game = Game([Will, Ben, Finn, Philip])
-game.showOdds() 
+
+# start game
+ournament = Tournament([Will, Ben, Finn, Philip])
+#give gui the game
+
+# tournament.showOdds() 
