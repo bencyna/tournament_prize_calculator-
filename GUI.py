c9315e7056c4a11e55f6316e589671a6afbc760f
Ben Cyna <benjamincyna@gmail.com>
2023-01-20 10:16:27 +1030

    odds generator fix

--- GUI.py
+++ GUI.py
@@ -8,6 +8,14 @@ class GUI:
         self.matches = []
         self.startGui()
 
+    def __buildTournament(self):
+        # add each match to the tournament tree
+        y = 50
+        x = 50
+        for round in self.matches:
+            for match in round:
+                print(match)
+
     def startGui(self):
         window = Tk()
         window.config(padx=50, pady=50, width=900, height=700, background='white')
@@ -17,8 +25,8 @@ class GUI:
         text = Text(width=50, font=('Times New Roman', 15, 'italic'), borderwidth=0)
         text.insert(INSERT, "Start typing...")
         text.pack(side=TOP, ipadx=30, ipady=10)
-
         canvas = Canvas(width=900, height=600, highlightthickness=0, background="white")
         canvas.pack()
         self.matches = self.tournament.getGames()
+        self.__buildTournament()
         window.mainloop()
--- Game.py
+++ Game.py
@@ -3,6 +3,8 @@ class Game:
     def __init__(self, player1: Person, player2: Person) -> None:
         self.p1 = player1
         self.p2 = player2
+        self.p1_odds = None
+        self.p2_odds = None
         self.__generateOdds()
         self.winner: Person = None
         self.score = [0, 0]
@@ -11,9 +13,10 @@ class Game:
         if self.p1 and self.p2:
             player1_chance = (self.p1.getSkill() / (self.p1.getSkill() + self.p2.getSkill())) * 100
             player2_chance = (self.p2.getSkill() / (self.p1.getSkill() + self.p2.getSkill())) * 100
-            return (player1_chance, player2_chance)
+            self.p1_odds = player1_chance
+            self.p2_odds = player2_chance
+            
         else:
-            print("Sorry, 2 players aren't here yet")
             return None
 
     def getP1(self) -> Person:
@@ -37,6 +40,16 @@ class Game:
 
     def setP2(self, player):
         self.p2 = player
+        self.__generateOdds()
 
     def setP1(self, player):
         self.p2 = player
+        self.__generateOdds()
+    
+    def getOdds(self):
+        if self.p1_odds != None:
+            return self.p1_odds, self.p2_odds
+        else:
+            print("sorry both players aren't here yet")
+        return
+
