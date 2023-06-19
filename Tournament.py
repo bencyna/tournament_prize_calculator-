5fb622f91fd89a0070703af45305d4307bcd5f65
Ben Cyna <benjamincyna@gmail.com>
2023-06-19 15:52:52 +0930

    remove return statement

--- Tournament.py
+++ Tournament.py
@@ -74,8 +74,7 @@ class Tournament:
         for game in self.matches:
             if p1 == game.getP1() and p2 == game.getP2():
                 return game
-         
-        return None
+          
     
        
     def getTournamentDets(self):
